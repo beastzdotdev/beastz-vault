bd1afbf34b3f3c563d1eb935c794acfc8364e5e4
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2023-11-08 01:22:24 +0400

    bug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+// import CryptoJS from 'crypto-js';
 import { Button } from '@blueprintjs/core';
 import { api, apiPure } from '../shared/api/api';
 
@@ -5,6 +6,73 @@ class AuthenticationPayloadResponseDto {
   accessToken: string;
   refreshToken: string;
   hasEmailVerified?: boolean;
+}
+
+async function decryptSync(encryptedText: string, masterkey: string) {
+  if (typeof encryptedText !== 'string') {
+    throw new Error(`Decryption error: String not found, found ${typeof encryptedText}`);
+  }
+
+  // Decode base64 encoded string to a Uint8Array
+  const encryptedData = new Uint8Array(Array.from(atob(encryptedText), c => c.charCodeAt(0)));
+
+  console.log('='.repeat(20));
+  console.log(encryptedData);
+
+  // Extract salt, iv, tag, and ciphertext from the encrypted data
+  const salt = encryptedData.slice(0, 64);
+  const iv = encryptedData.slice(64, 80);
+  const tag = encryptedData.slice(80, 96);
+  const ciphertext = encryptedData.slice(96);
+
+  // Derive the key using PBKDF2
+  const key = await crypto.subtle.importKey(
+    'raw',
+    new TextEncoder().encode(masterkey),
+    { name: 'PBKDF2' },
+    false,
+    ['deriveKey']
+  );
+
+  console.log('='.repeat(20));
+  console.log(key);
+
+  // name: 'aes-256-gcm' as const,
+
+  const derivedKey = await crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt: salt,
+      iterations: 2145,
+      hash: 'SHA-512',
+    },
+    key,
+    { name: 'AES-GCM', length: 256 },
+    false,
+    ['encrypt', 'decrypt']
+  );
+
+  console.log('='.repeat(20));
+  console.log('derivedKey');
+  console.log(derivedKey);
+
+  // Decrypt using AES-GCM
+  const decrypted = await crypto.subtle.decrypt(
+    {
+      name: 'AES-GCM',
+      iv: iv,
+      additionalData: salt,
+      tagLength: 128,
+    },
+    derivedKey,
+    ciphertext
+  );
+
+  console.log('='.repeat(20));
+  console.log(decrypted);
+
+  const decryptedText = new TextDecoder().decode(decrypted);
+  return decryptedText;
 }
 
 export const Index = () => {
@@ -24,18 +92,6 @@ export const Index = () => {
     );
 
     await Promise.all(promises);
-
-    // try {
-    //   const promises = [1, 2, 3].map(() => api.get('user/current'));
-    //   await Promise.all(promises);
-
-    //   const { data } = await api.get('user/current');
-    //   console.log(123);
-    //   console.log(data);
-    // } catch (error) {
-    //   console.log('='.repeat(20));
-    //   console.log(error);
-    // }
   };
 
   const getUser = async () => {
@@ -59,10 +115,19 @@ export const Index = () => {
       password: 'jsbyangtjt37*',
     });
 
-    // localStorage.setItem(constants.names.localStorageAccessToken, data.accessToken);
-    // localStorage.setItem(constants.names.localStorageRefreshToken, data.refreshToken);
-
     console.log(data);
+  };
+
+  const cryptoTest = async () => {
+    const encryptedText =
+      '5wz6l1gV0koGLwQiS+dw9Hcz/FjwRqjL0oF09jDvvA6xPexNQY0wXRuG9TYUkmaJxyvbBSCDwHEx6UWq7f0HLPBpYbeHgKOlykRiatb+5dzPh4fRw6PXB/fidO33TKIkTudjO94aYsdiK/8=';
+    const masterkey = 'test key';
+
+    // Usage
+    const x = await decryptSync(encryptedText, masterkey);
+
+    console.log('='.repeat(20));
+    console.log(x);
   };
 
   return (
@@ -72,6 +137,7 @@ export const Index = () => {
       <Button icon="log-out" intent="primary" text="get users" onClick={getUsers} />
       <Button icon="log-out" intent="warning" text="refresh" onClick={refresh} />
       <Button icon="log-out" intent="none" text="Login" onClick={login} />
+      <Button icon="log-out" intent="none" text="crypto test" onClick={cryptoTest} />
     </div>
   );
 };
