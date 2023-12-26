efb2b6854f59889b0a925553cdbd9b28ffe8924a
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2023-12-26 18:55:10 +0400

    resize on sidebar added

--- src/features/app.tsx
+++ src/features/app.tsx
@@ -1,12 +1,45 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from './shared/ui/sidebar';
-// import { TopBar } from './shared/ui/topbar';
 import { observer } from 'mobx-react-lite';
 import { useInjection } from 'inversify-react';
 import { SharedStore } from './shared/state/shared.store';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export const App = observer((): React.JSX.Element => {
   const sharedStore = useInjection(SharedStore);
+
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState(250);
+
+  const startResizing = useCallback(() => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = useCallback(
+    mouseMoveEvent => {
+      if (isResizing) {
+        setSidebarWidth(
+          mouseMoveEvent.clientX - (sidebarRef.current?.getBoundingClientRect()?.left ?? 0)
+        );
+      }
+    },
+    [isResizing]
+  );
+
+  useEffect(() => {
+    window.addEventListener('mousemove', resize);
+    window.addEventListener('mouseup', stopResizing);
+
+    return () => {
+      window.removeEventListener('mousemove', resize);
+      window.removeEventListener('mouseup', stopResizing);
+    };
+  }, [resize, stopResizing]);
 
   return (
     <>
@@ -15,7 +48,15 @@ export const App = observer((): React.JSX.Element => {
           {/* <TopBar /> */}
 
           <div className="flex">
-            <div className="w-[250px] bg-zinc-800 h-screen">
+            <div
+              className="bg-zinc-800 h-screen relative app-sidebar max-w-[600px] min-w-[300px]"
+              ref={sidebarRef}
+              style={{ width: sidebarWidth }}
+              onMouseDown={e => e.preventDefault()}
+            >
+              <div className="resize-bar" onMouseDown={startResizing}>
+                <div className="resize-line"></div>
+              </div>
               <Sidebar />
             </div>
 
