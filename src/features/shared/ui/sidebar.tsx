7c40745615c84bf2c296f9f3cc88a67d02264f10
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2023-12-26 21:53:27 +0400

    sidebar completed general ui

--- src/features/shared/ui/sidebar.tsx
+++ src/features/shared/ui/sidebar.tsx
@@ -15,6 +15,7 @@ import {
 import { useState } from 'react';
 import logo from '../../../assets/images/profile/doodle-man-1.svg';
 import { SidebarTree } from '../../../widgets/sidebar-tree';
+import { v4 as uuid } from 'uuid';
 
 type SidebarNodeInfo = TreeNodeInfo<{ link: string | null }>;
 
@@ -29,8 +30,18 @@ const INITIAL_STATE: SidebarNodeInfo[] = [
     id: 1,
     icon: 'folder-close',
     label: 'Folder 0',
-    childNodes: [],
+    childNodes: [
+      // eslint-disable-next-line no-constant-condition
+      ...(1 >= 1
+        ? Array.from({ length: 20 }, () => ({
+            id: uuid(),
+            icon: 'document' as const,
+            label: uuid(),
+          }))
+        : []),
+    ],
   },
+
   {
     id: 2,
     icon: 'folder-close',
@@ -50,6 +61,7 @@ const INITIAL_STATE: SidebarNodeInfo[] = [
         icon: 'document',
         label: 'Profile-backup.jpg',
       },
+
       {
         id: 3,
         label: 'Something',
@@ -101,99 +113,91 @@ export const Sidebar = () => {
   const [showFiles, setShowFiles] = useState(true);
 
   return (
-    <>
-      <div className="gorilla-profile flex items-center justify-between py-2 mt-2 mx-1.5 mb-1 cursor-pointer">
-        <div className="flex items-center">
-          <img width={24} height={24} src={logo} alt="" className="rounded-sm ml-1.5" />
+    <div className="flex flex-col h-full">
+      <div className="pt-2">
+        <div className="gorilla-profile flex items-center justify-between py-2 mx-1.5 mb-1 cursor-pointer">
+          <div className="flex items-center">
+            <img width={24} height={24} src={logo} alt="" className="rounded-sm ml-1.5" />
 
-          <p className="ml-2 font-medium">Giorgi Kumelashvili</p>
+            <p className="ml-2 font-medium">Giorgi Kumelashvili</p>
+          </div>
+
+          <Icon icon="expand-all" className="justify-self-end mr-2.5" />
         </div>
 
-        <Icon icon="expand-all" className="justify-self-end mr-2.5" />
-      </div>
-
-      <div>
-        <ButtonGroup
-          fill
-          minimal
-          vertical
-          alignText="left"
-          className="px-1.5 pb-1.5 gorilla-sidebar-buttons"
-        >
-          <Popover
-            content={
-              <Menu>
-                <MenuItem text="New" icon="document" />
-                <MenuItem text="Create" icon="folder-new" />
-                <MenuDivider />
-                <MenuItem text="Upload File" icon="document-open" />
-                <MenuItem text="Upload Folder" icon="folder-shared-open" />
-                <MenuDivider />
-                <MenuItem text="Gorilla doc (coming soon)" icon="document" />
-              </Menu>
-            }
-            placement="right-start"
+        <div>
+          <ButtonGroup
+            fill
+            minimal
+            vertical
+            alignText="left"
+            className="px-1.5 pb-1.5 gorilla-sidebar-buttons"
           >
-            <Button icon="plus" rightIcon="chevron-right" text="New" />
-          </Popover>
-          <Button icon="eye-open">Activity</Button>
-          <Button icon="people">Members</Button>
-          <Button icon="cog">Settings</Button>
-        </ButtonGroup>
+            <Popover
+              content={
+                <Menu>
+                  <MenuItem text="New" icon="document" />
+                  <MenuItem text="Create" icon="folder-new" />
+                  <MenuDivider />
+                  <MenuItem text="Upload File" icon="document-open" />
+                  <MenuItem text="Upload Folder" icon="folder-shared-open" />
+                  <MenuDivider />
+                  <MenuItem text="Gorilla doc (coming soon)" icon="application" />
+                </Menu>
+              }
+              placement="right-start"
+            >
+              <Button icon="plus" rightIcon="chevron-right" text="New" />
+            </Popover>
+            <Button icon="eye-open" text="Activity" />
+            <Button icon="updated" text="Recent" />
+            <Button icon="people" text="Members" />
+            <Button icon="inherited-group" text="Shared" />
+            <Button icon="cog" text="Settings" />
+          </ButtonGroup>
+        </div>
+
+        <div className="my-4"></div>
       </div>
 
-      <br />
-      <br />
-      <br />
-
-      <div
-        className="hover:bg-zinc-800 active:bg-zinc-700 w-fit ml-3"
-        onClick={() => setShowBookmarks(!showBookmarks)}
-      >
-        <p className="text-xs text-zinc-500 font-bold cursor-pointer">Bookmarks</p>
-      </div>
-
-      {showBookmarks && <SidebarTree state={INITIAL_STATE} />}
-
-      <div className="my-2"></div>
-
-      <div
-        className="hover:bg-zinc-800 active:bg-zinc-700 w-fit ml-3"
-        onClick={() => setShowFiles(!showFiles)}
-      >
-        <p className="text-xs text-zinc-500 font-bold cursor-pointer">Files</p>
-      </div>
-
-      {showFiles && <SidebarTree state={INITIAL_STATE2} />}
-
-      <div className="my-5"></div>
-
-      <div className="absolute bottom-0 w-full">
-        <ButtonGroup
-          fill
-          minimal
-          vertical
-          alignText="left"
-          className="px-1.5 pb-1.5 gorilla-sidebar-buttons"
+      <div className="flex-1 overflow-y-scroll">
+        <div
+          className="hover:bg-zinc-800 active:bg-zinc-700 w-fit ml-3"
+          onClick={() => setShowBookmarks(!showBookmarks)}
         >
-          <Button icon="inherited-group" text="Shared" />
-          <Button icon="updated" text="Recent" />
-          <Button icon="trash" text="Trash" />
-          <Button icon="ninja" text="AI (coming soon)" />
-          <Button icon="help" text="Support & Help" />
-        </ButtonGroup>
+          <p className="text-xs text-zinc-500 font-bold cursor-pointer">Bookmarks</p>
+        </div>
 
-        <Callout className="gorilla-callout !bg-zinc-900" title={'Storage'} icon="cloud">
-          <ProgressBar
-            animate={false}
-            stripes={false}
-            value={0.2}
-            className="mt-2 h-1 !bg-zinc-700"
-          />
+        {showBookmarks && <SidebarTree state={INITIAL_STATE} />}
 
-          <p className="bp5-text-muted text-[12.5px] mt-1.5">5.5 Gb used out of 20 Gb</p>
-        </Callout>
+        <div className="my-2"></div>
+
+        <div
+          className="hover:bg-zinc-800 active:bg-zinc-700 w-fit ml-3"
+          onClick={() => setShowFiles(!showFiles)}
+        >
+          <p className="text-xs text-zinc-500 font-bold cursor-pointer">Files</p>
+        </div>
+
+        {showFiles && <SidebarTree state={INITIAL_STATE2} />}
+
+        <div className="my-5"></div>
+
+        <div className="">
+          <ButtonGroup
+            fill
+            minimal
+            vertical
+            alignText="left"
+            className="px-1.5 pb-1.5 gorilla-sidebar-buttons"
+          >
+            <Button icon="trash" text="Trash" />
+            <Button icon="ninja" text="AI (coming soon)" />
+            <Button icon="data-connection" text="23.45 %" />
+            <Button icon="help" text="Support & Help" />
+          </ButtonGroup>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
