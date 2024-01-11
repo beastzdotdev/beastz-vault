419de3d7cca5132dfa0fe9be2cda2250cfb2bd39
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2024-01-12 01:32:48 +0400

    added runInAction for computedRequiresReaction

--- src/features/app-loader.ts
+++ src/features/app-loader.ts
@@ -3,6 +3,7 @@ import { ProfileController } from './profile/state/profile.controller';
 import { constants, ioc, UserApiService } from '../shared';
 import { SharedController } from './shared/state/shared.controller';
 import { ProfileStore } from './profile/state/profile.store';
+import { runInAction } from 'mobx';
 
 export const appLoader = async (_args: LoaderFunctionArgs) => {
   const profileController = ioc.getContainer().get(ProfileController);
@@ -10,17 +11,19 @@ export const appLoader = async (_args: LoaderFunctionArgs) => {
   const sharedController = ioc.getContainer().get(SharedController);
   const userApiService = ioc.getContainer().get(UserApiService);
 
-  if (!profileStore.user) {
-    //TODO get error and show appropriate messages, handle error
-    const { data } = await userApiService.getCurrentUser();
+  await runInAction(async () => {
+    if (!profileStore.user) {
+      //TODO get error and show appropriate messages, handle error
+      const { data } = await userApiService.getCurrentUser();
 
-    // Api call will make sure whether user is locked, blocked or not verified
-    sharedController.setShouldRender(data ? true : false);
+      // Api call will make sure whether user is locked, blocked or not verified
+      sharedController.setShouldRender(data ? true : false);
 
-    if (data) {
-      profileController.setUser(data);
+      if (data) {
+        profileController.setUser(data);
+      }
     }
-  }
+  });
 
   const url = new URL(_args.request.url);
 
