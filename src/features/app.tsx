0ba252aa4e08e84dcef5e5f068e72ec2af845d04
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2024-05-31 02:07:24 +0400

    started

--- src/features/app.tsx
+++ src/features/app.tsx
@@ -7,6 +7,9 @@ import { Sidebar } from './shared/widgets/sidebar';
 export const App = observer((): React.JSX.Element => {
   const sharedStore = useInjection(SharedStore);
 
+  //TODO: file view and listenet shold be here before outlet
+  //TODO: create separate component which returns nothing empty html
+
   return (
     <>
       {sharedStore.shouldRender && (
